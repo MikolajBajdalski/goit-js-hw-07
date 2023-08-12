b76d101f303dfa99dcfb13b9203fe718853f94ca
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-08-12 17:49:41 +0200

    idk

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+// import * as basicLightbox from 'basiclightbox'
+// import 'basiclightbox/dist/basicLightbox.min.css';
+
+const galleryList = document.querySelector('.gallery')
+
+const liTemplates = galleryItems.map((galleryItems)=> `
+<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItems.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItems.preview}"
+      data-source="${galleryItems.original}"
+      alt="${galleryItems.description}"
+    />
+  </a>
+</div>
+`);
+
+
+galleryList.insertAdjacentHTML("beforeend", liTemplates.join(""));
+
+galleryList.addEventListener('click', (e)=> {
+    e.preventDefault();
+
+    if (e.target.classList.contains('gallery__image')){
+        const largeImageUrl = e.target.dataset.source;
+
+        const instance = basicLightbox.create(`
+        <img src="${largeImageUrl}">
+      `);
+  
+      instance.show();
+    }
+});
