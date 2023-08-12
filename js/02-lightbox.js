61bf616b00670837540eccf1039ba33569ea095d
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-08-12 19:36:43 +0200

    ligthbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery')
+
+const liTemplates = galleryItems.map((galleryItems)=> `
+<li class="gallery__item">
+  <a class="gallery__link" href="${galleryItems.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItems.preview}"
+      data-source="${galleryItems.original}"
+      alt="${galleryItems.description}"
+    />
+  </a>
+</li>
+`);
+galleryList.insertAdjacentHTML("beforeend", liTemplates.join(""));
+
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+
+
